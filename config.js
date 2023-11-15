c6ab3679812ee1597084be4c27b7ef1f9998c78a
reettest20 <150687398+reettest20@users.noreply.github.com>
2023-11-15 07:11:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764082003', 'GURU', true],
-  ['94767969374', 'Ash', true], 
+  ['94767969374', 'GURU', true],
+  ['', 'Ash', fails], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['94767969374','94764082003'] 
-global.prems = ['94767969374', '94764082003', '']
-global.allowed = ['94767969374', '94764082003', '']
+global.mods = ['94767969374',''] 
+global.prems = ['94767969374', '', '']
+global.allowed = ['94767969374', '', '']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
